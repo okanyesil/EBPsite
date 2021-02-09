f2c4ee14571e8ff396348bb6e5e977b9b0151a0a
Okan Yeşiloğlu <myesiloglu0@gmail.com>
2021-02-10 00:07:09 +0300

    gÃmain-page görünümü oluşturulmaya başlandı

--- src/libs/modules/main-page/main-page.module.ts
+++ src/libs/modules/main-page/main-page.module.ts
@@ -3,13 +3,15 @@ import { CommonModule } from '@angular/common';
 
 import { MainPageRoutingModule } from './main-page-routing.module';
 import { MainPageContainerComponent } from './containers/main-page-container/main-page-container.component';
+import {NgbCarouselModule} from '@ng-bootstrap/ng-bootstrap';
 
 
 @NgModule({
   declarations: [MainPageContainerComponent],
   imports: [
     CommonModule,
-    MainPageRoutingModule
+    MainPageRoutingModule,
+    NgbCarouselModule
   ]
 })
 export class MainPageModule { }
