9e48007c95d7159c01fedc44146a91b7b593f421
Okan Yeşiloğlu <myesiloglu0@gmail.com>
2021-02-09 21:48:35 +0300

    routing eklendi

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,10 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'sepetim', loadChildren: () => import('../libs/modules/shopping-cart/shopping-cart.module')},
+  {path: 'main-page', loadChildren: () => import('../libs/modules/main-page/main-page.module')}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
