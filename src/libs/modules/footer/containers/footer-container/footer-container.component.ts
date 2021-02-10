108be2ba7587f8420ae565e33bd7b89ed37b7db8
Okan Yeşiloğlu <myesiloglu0@gmail.com>
2021-02-10 21:30:58 +0300

    navbar footer container eklendi

---
+++ src/libs/modules/footer/containers/footer-container/footer-container.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-footer-container',
+  templateUrl: './footer-container.component.html',
+  styleUrls: ['./footer-container.component.scss']
+})
+export class FooterContainerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/libs/modules/navbar/containers/navbar-container/navbar-container.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-navbar-container',
+  templateUrl: './navbar-container.component.html',
+  styleUrls: ['./navbar-container.component.scss']
+})
+export class NavbarContainerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
