08a94852889d539c27c18f512d203047535a5d0b
esmanurkaraca <esma.krc.12@gmail.com>
2021-02-11 15:59:50 +0300

    owl-carousel eklendi

--- src/libs/modules/main-page/containers/main-page-container/main-page-container.component.ts
+++ src/libs/modules/main-page/containers/main-page-container/main-page-container.component.ts
@@ -1,11 +1,37 @@
 import { Component, OnInit } from '@angular/core';
-
+import { OwlOptions } from 'ngx-owl-carousel-o';
 @Component({
   selector: 'app-main-page-container',
   templateUrl: './main-page-container.component.html',
   styleUrls: ['./main-page-container.component.scss']
 })
 export class MainPageContainerComponent implements OnInit {
+
+
+  customOptions:OwlOptions  = {
+    loop: true,
+    mouseDrag: true,
+    touchDrag: true,
+    pullDrag: true,
+    dots: true,
+    navSpeed: 700,
+    navText: ['', ''],
+    responsive: {
+      0: {
+        items: 1
+      },
+      400: {
+        items: 2
+      },
+      740: {
+        items: 3
+      },
+      940: {
+        items: 4
+      }
+    },
+    nav: true
+  }
 
   constructor() { }
 
