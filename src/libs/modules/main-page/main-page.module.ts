2e0dbd7a05d80ad04d3660e9cf8fe462a8dbd09a
Okan Yeşiloğlu <myesiloglu0@gmail.com>
2021-02-11 17:40:55 +0300

    hatalar giderildi

--- src/libs/modules/main-page/main-page.module.ts
+++ src/libs/modules/main-page/main-page.module.ts
@@ -5,9 +5,10 @@ import { MainPageRoutingModule } from './main-page-routing.module';
 import { MainPageContainerComponent } from './containers/main-page-container/main-page-container.component';
 import {NgbCarouselModule} from '@ng-bootstrap/ng-bootstrap';
 import { CarouselModule } from 'ngx-owl-carousel-o';
+import {NavbarContainerComponent} from '../navbar/containers/navbar-container/navbar-container.component';
 
 @NgModule({
-  declarations: [MainPageContainerComponent],
+    declarations: [MainPageContainerComponent, NavbarContainerComponent],
   imports: [
     CommonModule,
     MainPageRoutingModule,
