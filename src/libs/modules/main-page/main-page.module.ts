fe1bd62bc5a0644f76b4c50d129fd931035e3de3
esmanurkaraca <esma.krc.12@gmail.com>
2021-02-11 15:59:50 +0300

    owl-carousel eklendi

--- src/libs/modules/main-page/main-page.module.ts
+++ src/libs/modules/main-page/main-page.module.ts
@@ -4,14 +4,15 @@ import { CommonModule } from '@angular/common';
 import { MainPageRoutingModule } from './main-page-routing.module';
 import { MainPageContainerComponent } from './containers/main-page-container/main-page-container.component';
 import {NgbCarouselModule} from '@ng-bootstrap/ng-bootstrap';
-
+import { CarouselModule } from 'ngx-owl-carousel-o';
 
 @NgModule({
   declarations: [MainPageContainerComponent],
   imports: [
     CommonModule,
     MainPageRoutingModule,
-    NgbCarouselModule
+    NgbCarouselModule,
+    CarouselModule ,
   ]
 })
 export class MainPageModule { }
