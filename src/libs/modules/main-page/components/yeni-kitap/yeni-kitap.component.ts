d512d90cc36967405745ac22dd1a495b3c821d5a
Okan Yeşiloğlu <myesiloglu0@gmail.com>
2021-02-20 21:52:25 +0300

    arama çubuğu eklendi.

--- src/libs/modules/main-page/components/yeni-kitap/yeni-kitap.component.ts
+++ src/libs/modules/main-page/components/yeni-kitap/yeni-kitap.component.ts
@@ -1,11 +1,14 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {BookService} from '../../services/book.service';
-import {map, mergeMap} from 'rxjs/operators';
+import {catchError, map, mergeMap, switchMap, take} from 'rxjs/operators';
 import {IBookModel} from '../../models/book.model';
 import {DomSanitizer} from '@angular/platform-browser';
 import {AngularFireStorage} from '@angular/fire/storage';
 import {ShoppingCartDbService} from '../../services/shopping-cart-db.service';
 import {MatSnackBar} from '@angular/material/snack-bar';
+import {AngularFireAuth} from '@angular/fire/auth';
+import {from} from 'rxjs';
+import {AngularFirestore} from '@angular/fire/firestore';
 
 @Component({
   selector: 'app-yeni-kitap',
@@ -16,14 +19,32 @@ export class YeniKitapComponent implements OnInit {
   @Input()
   bookInfo: IBookModel;
   discountPercentega: number;
-  constructor(private db: ShoppingCartDbService, private snackBar: MatSnackBar) { }
+  constructor(private db: ShoppingCartDbService,
+              private snackBar: MatSnackBar,
+              private auth: AngularFireAuth,
+              private firestore: AngularFirestore
+  ) { }
 
   ngOnInit(): void {
     this.discountPercentega = Math.ceil((this.bookInfo.price * 100) / this.bookInfo.oldPrice);
   }
-  deneme(bilgi) {
-    this.db.getData().subscribe(data => {
-      console.log(data);
+  addToCart(bilgi) {
+    this.auth.user.pipe(
+      take(1),
+      switchMap((user) => {
+        return from(this.firestore.collection('shoppingCart').add({...bilgi, userId: user.uid}));
+      }),
+      catchError((err) => {
+        const user = localStorage.getItem('user');
+        if (user === 'null') {
+          this.snackBar.open('Kitap Eklemek için lütfen giriş yapın', null, {duration: 2000});
+        } else {
+          this.snackBar.open('Beklenmedik bir hata oluştu', null, {duration: 2000});
+
+        }
+        return err;
+      })
+    ).subscribe(() => {
       this.snackBar.open('Ürün Başarıyla sepete eklendi', null, { duration: 2000});
     });
   }
--- src/libs/modules/navbar/containers/navbar-container/navbar-container.component.ts
+++ src/libs/modules/navbar/containers/navbar-container/navbar-container.component.ts
@@ -3,6 +3,10 @@ import {NgbModal} from '@ng-bootstrap/ng-bootstrap';
 import {AngularFireAuth} from '@angular/fire/auth';
 import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 import {MatSnackBar} from '@angular/material/snack-bar';
+import {SetUserService} from '../../services/set-user.service';
+import {catchError, filter, switchMap, take} from 'rxjs/operators';
+import {from} from 'rxjs';
+import {AngularFirestore} from '@angular/fire/firestore';
 
 @Component({
   selector: 'app-navbar-container',
@@ -10,6 +14,7 @@ import {MatSnackBar} from '@angular/material/snack-bar';
   styleUrls: ['./navbar-container.component.scss']
 })
 export class NavbarContainerComponent implements OnInit {
+  userInfo: any;
   signInForm = this.fb.group({
     email: this.fb.control('', [Validators.required, Validators.email]),
     password: this.fb.control('', [Validators.required])
@@ -18,15 +23,19 @@ export class NavbarContainerComponent implements OnInit {
   signUpForm = this.fb.group({
     name: this.fb.control('', [Validators.required]),
     email: this.fb.control('', [Validators.required, Validators.email]),
+    lastName: this.fb.control('', [Validators.required]),
     password: this.fb.control('', [Validators.required])
   });
   constructor(private modalService: NgbModal,
-              private auth: AngularFireAuth,
+              public auth: AngularFireAuth,
               private fb: FormBuilder,
-              private snackbar: MatSnackBar
+              private snackbar: MatSnackBar,
+              private saveUser: SetUserService,
+              private firestore: AngularFirestore
   ) { }
 
   ngOnInit(): void {
+    this.userInfo = {userId: '', lastName: '', email: '', name: ''};
   }
   open(content) {
     this.modalService.open(content, {size: 'lg'});
@@ -35,17 +44,61 @@ export class NavbarContainerComponent implements OnInit {
     this.auth.signInWithEmailAndPassword(this.signInForm.value.email, this.signInForm.value.password)
       .then(() => {
         this.snackbar.open('Giriş Başarılı...', null, {duration: 2000});
+        this.getProfile().subscribe(data => {
+          this.userInfo = data;
+          localStorage.setItem('user', JSON.stringify(this.userInfo));
+        });
         this.modalService.dismissAll();
+        this.signInForm.reset();
       })
       .catch(() => {
         this.snackbar.open('Bazı hatalar Oluştu...', null, {duration: 2000});
+        this.auth.authState.subscribe(console.log);
+        this.auth.user.subscribe(data => console.log('user data ' + data));
       });
+  }
+  logOut() {
+    this.auth.signOut().then(() => {
+      localStorage.setItem('user', null);
+      this.snackbar.open('Çıkış Başarılı', null, {duration: 200});
+    });
   }
 
   signUp() {
     this.auth.createUserWithEmailAndPassword(this.signUpForm.value.email, this.signUpForm.value.password)
-      .then(data => console.log(data))
+      .then(() => {
+        this.auth.user.pipe(
+          take(1),
+          switchMap((user) => {
+            return from(this.firestore
+              .doc(`userProfile/${user.uid}`)
+              .set({...{email: this.signUpForm.value.email,
+                  name: this.signUpForm.value.name, lastName: this.signUpForm.value.lastName,
+                  userId: this.signUpForm.value.uid}, userId: user.uid}));
+          }),
+          catchError((error) => {
+            this.snackbar.open('Eroor', null, { duration: 2000});
+            return error;
+          })
+        ).subscribe(() => {
+          this.modalService.dismissAll();
+          this.signUpForm.reset();
+          this.snackbar.open('Kayıt Başarılı..', null, {duration: 200});
+        });
+
+      })
       .catch(data => console.log(data));
+  }
+  getProfile() {
+    return this.auth.user.pipe(
+      take(1),
+      switchMap((user) => {
+        return this.firestore.doc(`userProfile/${user.uid}`).valueChanges()
+          .pipe(
+            filter(value => !!value)
+          );
+      })
+    );
   }
 
 }
